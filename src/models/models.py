7c94bab2c68a94b1d30104834e34e337a8965abc
IsmailHatim <ismoil59600@gmail.com>
2025-03-21 14:14:22 +0100

    Added ResNet model

--- src/models/models.py
+++ src/models/models.py
@@ -1,5 +1,3 @@
-
-
 import torch
 import torch.nn as nn
 import torchvision.models as models
@@ -13,6 +11,30 @@ class DenseNetClassifier(nn.Module):
 
         num_ftrs = self.model.classifier.in_features
         self.model.classifier = nn.Sequential(
+            nn.Linear(num_ftrs, 1),
+            nn.Sigmoid()
+        ).to(device)
+        
+        self.device = device
+    
+    def forward(self, x):
+        return self.model(x)
+    
+    def save_model(self, path):
+        torch.save(self.state_dict(), path)
+    
+    def load_model(self, path):
+        self.model.load_state_dict(torch.load(path, map_location=self.device))
+
+class ResNetClassifier(nn.Module):
+    def __init__(self, pretrained=True, device=torch.device("cpu")):
+        super(ResNetClassifier, self).__init__()
+        
+        weights = "DEFAULT" if pretrained else None
+        self.model = models.resnet50(weights=weights).to(device)
+
+        num_ftrs = self.model.fc.in_features
+        self.model.fc = nn.Sequential(
             nn.Linear(num_ftrs, 1),
             nn.Sigmoid()
         ).to(device)
