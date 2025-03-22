6b117bb8144a6110c62d2a252138d91a2e86fede
IsmailHatim <ismoil59600@gmail.com>
2025-03-22 12:34:36 +0100

    Fixed lime for main run task

--- run_explanation.py
+++ run_explanation.py
@@ -5,6 +5,7 @@ import torch
 import importlib
 import time
 import torchvision.transforms as transforms
+from skimage.segmentation import mark_boundaries
 from argparse import ArgumentParser
 from src.data.dataset import DAGMDataset
 from src.models.models import DenseNetClassifier
@@ -38,42 +39,57 @@ def main(args):
     model.eval()
     output = model(image.to(device))
     prob = output.item()
-
+    
     start_time = time.perf_counter()
     explanation, explanation_thresholded = method(model.model, image, device, threshold=args.threshold, conv_layer_index=args.conv_layer_index)
     end_time = time.perf_counter()
 
     execution_time = end_time - start_time
+    if args.method != "lime":
+        iou_score = compute_iou_score(explanation_thresholded, label_image)
+        f1_score = compute_f1_score(explanation_thresholded, label_image)
+        # auc_score = compute_auc_score(explanation, label_image)
+        
+        if args.method == 'saliency':
+            image = image.detach()
+        
+        print("+" + "-" * 50 + "+")
+        print(f'True Label: {label}')
+        print(f"IoU Score : {iou_score}")
+        print(f"F1 Score : {f1_score}")
+        # print(f"ROC AUC Score : {auc_score}")
+        print(f"Execution Time : {execution_time:.4f} seconds")
+        print("+" + "-" * 50 + "+")
 
-    iou_score = compute_iou_score(explanation_thresholded, label_image)
-    f1_score = compute_f1_score(explanation_thresholded, label_image)
-    # auc_score = compute_auc_score(explanation, label_image)
-    
-    if args.method == 'saliency':
-        image = image.detach()
-    
-    print("+" + "-" * 50 + "+")
-    print(f'True Label: {label}')
-    print(f"IoU Score : {iou_score}")
-    print(f"F1 Score : {f1_score}")
-    # print(f"ROC AUC Score : {auc_score}")
-    print(f"Execution Time : {execution_time:.4f} seconds")
-    print("+" + "-" * 50 + "+")
+        _, ax = plt.subplots(1, 4, figsize=(10, 5))
+        ax[0].imshow(image.cpu().squeeze().permute(1, 2, 0))
+        ax[0].set_title("Input Image")
 
-    _, ax = plt.subplots(1, 4, figsize=(10, 5))
-    ax[0].imshow(image.cpu().squeeze().permute(1, 2, 0))
-    ax[0].set_title("Input Image")
+        ax[1].imshow(image.cpu().squeeze().permute(1, 2, 0))
+        ax[1].imshow(explanation, cmap='jet', alpha=0.5)
+        ax[1].set_title(f"{args.method.capitalize()} Prob: {prob*100:.1f}%")
+        ax[1].legend()
 
-    ax[1].imshow(image.cpu().squeeze().permute(1, 2, 0))
-    ax[1].imshow(explanation, cmap='jet', alpha=0.5)
-    ax[1].set_title(f"{args.method.capitalize()} Prob: {prob*100:.1f}%")
-    ax[1].legend()
+        ax[2].imshow(explanation_thresholded, cmap='gray')
+        ax[2].set_title(f"Thresholded Mask")
 
-    ax[2].imshow(explanation_thresholded, cmap='gray')
-    ax[2].set_title(f"Thresholded Mask")
+        ax[3].imshow(label_image.squeeze().permute(1, 2, 0))
+        ax[3].set_title(f"Ground Truth")
+    else:
 
-    ax[3].imshow(label_image.squeeze().permute(1, 2, 0))
-    ax[3].set_title(f"Ground Truth")
+        print("+" + "-" * 50 + "+")
+        print(f"Execution Time : {execution_time:.4f} seconds")
+        print("+" + "-" * 50 + "+")
+        print(f'True Label: {label}')
+
+        fig, ax = plt.subplots(1, 2, figsize=(10, 5))
+        ax[0].imshow(image.cpu().squeeze().permute(1, 2, 0))
+        ax[0].set_title("Input Image")
+        
+        ax[1].imshow(mark_boundaries(explanation, explanation_thresholded))
+        ax[1].set_title("LIME Explanation")
+        
+        plt.show()
 
     plt.show()
 
